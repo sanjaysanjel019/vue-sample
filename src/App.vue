e07181e6e88f68890d653361ba5a96ba5d3474fe
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2020-11-06 07:09:17 +0545

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="testvue">
+    <span class="text-4xl capitalize font-semibold"> Popular Destinations</span>
+    <div class="flex justify-center align-center flex-row">
+    <Card :destinations="destinations" class="p-4 flex" >
+      
+    </Card>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Card from './components/Card'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    Card
+  },
+  data () {
+    return {
+       // eslint-disable-next-line no-unused-labels
+    destinations:[
+      {name:'Tilicho Lake',
+       description:'A beautiful lake with thousanads of Lorem ',
+       image:'https://www.himalayadarshantreks.com/wp-content/uploads/2018/08/Tilicho-Lake-Short-Trek-Tilicho-Lake-Trek-848x525.jpg'
+      },
+
+      {name:'Rani Pokhari',
+       description:'A strem of beautiful pearl filled water droplets that lets you win. ',
+       image:'https://www.nepalitimes.com/wp-content/uploads/2020/07/Building-back-Rani-Pokhari-better-NT-6.jpg'
+      },
+
+      {name:'Beautiful Pokhara',
+       description:'A heaven beneath the mountains that spark joy each time you visit. ',
+       image:'https://cdn.kimkim.com/files/a/content_articles/featured_photos/0a99949f58bc92145369e8363c64c33bfd85f819/big-68acc559aad8def0f326f4d62e8faf78.jpg'
+      }
+    ]
   }
+    }
+   
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+.testvue{
+  display: grid;
+  width: 100%;
+  place-items: center;
+  width:100%;
+  margin-top:200px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class=" border-solid  border-1 mx-2 border-gray-600 bg-gray-200 shadow-lg h-64 border-gray-800 p-5 w-64 hover:shadow-xl hover:cursor-pointer" v-for="destination in destinations" :key="destination.name">
+            <h3 class="font-medium font-2xl">{{destination.name}}</h3>
+            <img :src="destination.image">
+            <slot></slot>
+            <p> {{destination.description}}</p>
+        
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Card',
+        props:['destinations']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
